669c6cf4d97cd4a4ad2e9ea1e982b1fc857e1a6c
mingyueliang <mingyuex.liang@intel.com>
2022-07-12 14:19:54 +0800

    Add plus and minus image

--- src/viewlayout.ts
+++ src/viewlayout.ts
@@ -1,35 +1,20 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-// import { resolve, basename, dirname, join, extname} from 'path';
 import * as os from 'os';
 import * as child_process from 'child_process';
-import { stderr, stdout } from 'process';
 import * as path from 'path';
-import { SilentReporter } from '@vscode/test-electron';
-
 
 
 let webviewPanel = new Map();
 let pythonInterpreter = 'py -3';
 let fmmtPath = '';
+let fileTypeList = ["fd", "fv", "ffs", "sec"];
 
-function readFile(filePath: string){
-    return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-}
-
-function isFileExisted(filePath: string) {
-    return new Promise((resolve, reject) => {
-        fs.access(filePath, (err) => {
-            if (err) {
-                reject(false);
-            } else {
-                resolve(true);
-            }
-        });
-    });
-}
-
-
+/**
+ * @description Get real path
+ * @param file 
+ * @returns 
+ */
 function getHackPath(file:string){
     if (isWinOS()) {
         return file.substring(1);
@@ -38,20 +23,39 @@ function getHackPath(file:string){
 }
 
 /**
- * @description get system platform
+ * @description Determine whether it is a windows system
+ * @returns boolean
  */
 function isWinOS() {
 	return os.platform() === 'win32';
 }
-  
+
+/**
+ * @description Determine whether it is a MacOs system
+ * @returns boolean
+ */
 function isMacOS() {
 	return os.platform() === 'darwin';
 }
 
+/**
+ * @description Determine whether it is a Linux system
+ * @returns boolean
+ */
 function isLinuxOS() {
 	return os.platform() === 'linux';
 }
 
+/**
+ * @descriotion Get webview content.
+ * @param outName 
+ * @param jsonPath 
+ * @param filePath 
+ * @param fileType 
+ * @param plusPath 
+ * @param minuspath 
+ * @returns 
+ */
 function getWebviewContent(outName?: string, jsonPath?: vscode.Uri, filePath?: string, fileType?: string, plusPath?: string, minuspath?:string): string {
     return (
     `
@@ -97,19 +101,13 @@ function getWebviewContent(outName?: string, jsonPath?: vscode.Uri, filePath?: s
     <body>
         <div>
             <ul class="menu">
-                <li>Modules</li>
                 <li>FV Info</li>
-                <li>GUID xref</li>
                 <li id="uploadfile">Add FV</li>
                 <li onclick="deleteLiNode()">Clear</li>
-                <li>export</li>
-                <li>Raw Export</li>
-                <li>Extract Data</li>
-                <li>Extract Raw</li>
             </ul>
         </div>
         <div id="box"></div>
-        <input type="file" id="file" style="display:none" accept=".fd,.Fv, .fv,.ffs,.sec" multiple="multiple">
+        <input type="file" id="file" style="display:none" accept=".fd,.Fv, .fv,.ffs,.sec">
         
         <script type="text/javascript">
             const vscode = acquireVsCodeApi();
@@ -117,7 +115,6 @@ function getWebviewContent(outName?: string, jsonPath?: vscode.Uri, filePath?: s
             // revice message from vscode
             window.addEventListener('message', event => {
                 const message = event.data;
-                console.log('webview接收到的消息: '+message.files)
                 var files = message.files.split(";");
                 for (var i=0;i<files.length; i++){
                     var fileObj = files[i].split(",")
@@ -160,11 +157,11 @@ function getWebviewContent(outName?: string, jsonPath?: vscode.Uri, filePath?: s
                             if (this == event.target) {
                                 if ($(this).children().is(':hidden')){
                                     $(this)
-                                    // .css('list-style-image', 'url(${minuspath})')
+                                    .css('list-style-image', 'url(${minuspath})')
                                     .children().show()
                                 } else {
                                     $(this)
-                                    // .css('list-style-image', 'url(${plusPath})')
+                                    .css('list-style-image', 'url(${plusPath})')
                                     .children().hide()
                                 }
                             }
@@ -180,7 +177,7 @@ function getWebviewContent(outName?: string, jsonPath?: vscode.Uri, filePath?: s
             
                         $('li:has(ul)').css({
                             cursor: 'default',
-                            // 'list-style-image': 'url(${plusPath})'
+                            'list-style-image': 'url(${plusPath})'
                         })
                     })            
                 }
@@ -218,8 +215,14 @@ function getWebviewContent(outName?: string, jsonPath?: vscode.Uri, filePath?: s
                             } else if (fileType.search(/fd|fv/gi) !== -1){
                                 name = 'FvNameGuid'
                             }
+                            if (fileType.search(/sec/gi !== -1)) {
+                                setInnerText(fvli, fvObj['Name']+' Size='+fvObj['Size']+' Offset='+fvObj['Offset']+' Files='+fvObj['FilesNum'])
+                            }
                             setInnerText(fvli, fvObj['Name']+'('+fvObj[name]+')'+' Size='+fvObj['Size']+' Offset='+fvObj['Offset']+' Files='+fvObj['FilesNum'])
 
+                            if (fvObj['Files'] === undefined) {
+                                return true
+                            }
                             // get ffs in fv
                             var ffsbox = document.getElementById(idName)
                             var ffsul = document.createElement('ul')
@@ -276,7 +279,6 @@ function getWebviewContent(outName?: string, jsonPath?: vscode.Uri, filePath?: s
             function GenNonDuplicateID(){
                 return Math.random().toString(36).substr(2)
             }
-
         </script>
     </body>
     </html>`);
@@ -284,18 +286,26 @@ function getWebviewContent(outName?: string, jsonPath?: vscode.Uri, filePath?: s
 
 module.exports =function(context: vscode.ExtensionContext){
     context.subscriptions.push(vscode.commands.registerCommand('vscode-view-fd-fv-ffs-sec-layout-extension.viewlayout', (uri) => {
-        vscode.window.showInformationMessage("view layout");
+        vscode.window.showInformationMessage("Loading current file layout......");
         var filePath = getHackPath(uri.path);
         var outName = path.basename(filePath);
         var index = outName.lastIndexOf('.');
         var fileType = outName.substring(index+1);
+        if (fileTypeList.indexOf(fileType.toLowerCase()) === -1) {
+            vscode.window.showErrorMessage("Error: Wrong file type, Please re select!!!");
+            return;
+        }
         var sourceFilePath = path.join(path.dirname(path.dirname(__filename)), `./Layout_${outName}.json`);
 
         createPanel(context, outName, sourceFilePath, filePath, fileType);        
     }));
 };
 
-
+/**
+ * @description Call FMMT tool to parser current file.
+ * @param sourceFilePath 
+ * @param filePath 
+ */
 async function showTermianl(sourceFilePath:string, filePath:string) {
     fs.stat(sourceFilePath, function(err, stat) {
         if (stat&&stat.isFile()){
@@ -307,16 +317,6 @@ async function showTermianl(sourceFilePath:string, filePath:string) {
             fmmtPath = path.join(path.dirname(__dirname), 'utils/FMMT2/FMMT.py');
             var commands = `${pythonInterpreter} ${fmmtPath} -v ${filePath} -l json`;
             
-            // run FMMT.py in terminal
-            // var _terminal = vscode.window.createTerminal(
-            //     {
-            //         name: 'view-layout',
-            //         cwd: path.dirname(path.dirname(__filename))
-            //     }
-            // );
-            // _terminal.sendText(commands);
-            // _terminal.show();
-
             // create process to run FMMT.py
             let cwd = path.join(path.dirname(__dirname));
             const output = child_process.execSync(commands, {cwd: cwd});
@@ -325,6 +325,14 @@ async function showTermianl(sourceFilePath:string, filePath:string) {
     });
 };
 
+/**
+ * @description Create webview panel
+ * @param context 
+ * @param outName Current file name
+ * @param sourceFilePath 
+ * @param filePath 
+ * @param fileType 
+ */
 async function createPanel(context: vscode.ExtensionContext, outName:string, sourceFilePath:string, filePath:string, fileType:string) {
     var column = vscode.window.activeTextEditor?vscode.window.activeTextEditor.viewColumn:undefined;
         // If we already have a panel, show it.
@@ -350,13 +358,10 @@ async function createPanel(context: vscode.ExtensionContext, outName:string, sou
             var plusPath = panel.webview.asWebviewUri(vscode.Uri.file(path.join(context.extensionPath, `img/plus.png`))).toString();
             var minusPath = panel.webview.asWebviewUri(vscode.Uri.file(path.join(context.extensionPath, `img/minus.png`))).toString();
 
-
-
             panel.webview.html = getWebviewContent(outName, jsonPath, filePath, fileType, plusPath, minusPath);
             
             // revice message from webview
             panel.webview.onDidReceiveMessage(async message => {
-                console.log(message);
                 var resFiles = [];
                 var files = message.filepath.split(";");
                 for (let index = 0; index < files.length; index++) {
